c396e98c502cb003e55c777930bfa1ca6cea5db4
nepalrohan <rohannepal098@gmail.com>
2024-12-30 17:14:48 +0545

    Readonly in ts

--- src/index.ts
+++ src/index.ts
@@ -26,3 +26,48 @@ type UpdateoptionalProp = Partial<UpdateProp>
 //now the updateprop has converted to an optional prop 
 
 
+
+//3. Readonly
+// if we have to make object or array inside fiels as redonly we can use this in ts.
+
+/*eg: const user =- {
+name:"asdf",
+age:21
+}
+
+user.name = "asdfhaslkjdfh"
+
+
+or 
+
+const a = [1, 2, 3, 4,]
+a[0] = 2;//it doesnot throw error
+
+
+ Here, they are const but inside values can be updated 
+ it cant be updated as:
+ const a =[1, 2,4 ]
+a=[4,6,7]//This cannot be done it throw an error
+ */
+
+type ReadonlyUser = Readonly<User>
+//now User objects value cannot be changed inside which make them readonly
+//or
+type Person ={
+   readonly name:string;
+    readonly age:number;
+}
+const user:Person = {
+    name:"asdf",
+    age:21
+}
+//or we can do as
+type Person2 ={
+    readonly name:string;
+     readonly age:number;
+ }
+const user2:Readonly<Person2> = {
+    name:"asdf",
+    age:21
+}
+// user.name = "asdfhaslkjdfh" //it throws error as name is readonly
